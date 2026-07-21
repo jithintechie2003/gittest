404d753a7e03ad688237966d948f02ebd7f08d4f
Jithin k <jithintechie@gmail.com>
2026-07-21 11:49:23 +0530

    updates js file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let count = 0;
-let myButton = document.getElementById("my-Button");
-let numberText = document.getElementById(number - text);
+let myButton = document.getElementById("my-button");
+let numberText = document.getElementById("number-text");
 
 myButton.addEventListener("click", function () {
   count = count + 1;
